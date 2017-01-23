62449920331dde433abe00a6f9c3acfa09ddb42e
mari <muthu.itian@gmail.com>
2017-01-23 09:24:43 -0500

    Add new Activity Add Information and Realm Initialization

--- app/src/main/java/com/tech42/mari/recyclerviewtest/Information.java
+++ app/src/main/java/com/tech42/mari/recyclerviewtest/Information.java
@@ -1,11 +1,40 @@
 package com.tech42.mari.recyclerviewtest;
 
+import android.graphics.Bitmap;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import io.realm.annotations.Required;
+
 /**
  * Created by mari on 1/23/17.
  */
 
-public class Information {
+public class Information extends RealmObject {
 
-    int imageid;
+    @PrimaryKey
+    Bitmap imageid;
+
+    @Required
     String text;
+
+    public Bitmap getImageid()
+    {
+        return imageid;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setImageid(Bitmap imageid)
+    {
+        this.imageid=imageid;
+    }
+    public void setText(String text)
+    {
+        this.text=text;
+    }
+
 }
